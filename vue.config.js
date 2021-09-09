513b1c3e81a1ba3f68a17e2259fc6594e4e612a8
SanjayDevTech <sureshsanjay805@gmail.com>
2021-09-09 11:51:16 +0530

    added firebase config

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,22 @@
 module.exports = {
-  chainWebpack: config=>{
-    config.plugins.delete('prefetch')
+  chainWebpack: config => {
+    config.plugins.delete('prefetch');
   },
-  "transpileDependencies": [
-    "vuetify"
-  ],
+  transpileDependencies: ['vuetify'],
   productionSourceMap: false,
   pwa: {
-    name: 'Aura Main',
+    name: 'GDSC SREC',
     workboxPluginMode: 'InjectManifest',
     themeColor: '#4A90E2',
     msTileColor: '#4A90E2',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
     manifestOptions: {
-      background_color: "#ffffff"
+      background_color: '#ffffff',
     },
     workboxOptions: {
       swSrc: './src/sw.js',
       swDest: 'service-worker.js',
     },
-  }
-}
+  },
+};
